1f511bb2dc080043738abfca6a5989d2078d0bed
diego0096 <dfqz93@hotmail.com>
2020-07-15 17:05:33 +0000

    machine learning

---
+++ math/0x00-linear_algebra/2-size_me_please.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+def matrix_shape(matrix):
+    shape = []
+    try:
+        while(len(matrix) > 0):
+            shape.append(len(matrix))
+            matrix = matrix[0]
+    except TypeError:
+        pass
+    return shape
---
+++ math/0x00-linear_algebra/3-flip_me_over.py
@@ -0,0 +1,5 @@
+#!usr/bin/env python3
+
+def matrix_transpose(matrix):
+
+    return [[row[zero] for row in matrix] for zero in range(len(matrix[0]))]
