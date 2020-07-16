f5f718f87b75fb92b2af2951aa4596f5affdded3
diego0096 <dfqz93@hotmail.com>
2020-07-16 22:45:48 +0000

    twelve

---
+++ math/0x00-linear_algebra/12-bracin_the_elements.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""numpy arithmetic"""
+
+
+def np_elementwise(mat1, mat2):
+    """return matrix operations"""
+    return (mat1 + mat2, mat1 - mat2, mat1 * mat2, mat1 / mat2)
