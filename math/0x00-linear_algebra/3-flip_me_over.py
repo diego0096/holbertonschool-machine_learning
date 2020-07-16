294c0f4199a8e6398fcfe3a14da12c1cbb0a23d5
diego0096 <dfqz93@hotmail.com>
2020-07-16 22:49:00 +0000

    commentary

--- math/0x00-linear_algebra/3-flip_me_over.py
+++ math/0x00-linear_algebra/3-flip_me_over.py
@@ -1,6 +1,8 @@
 #!usr/bin/env python3
+"""transpose the matrix"""
 
 
 def matrix_transpose(matrix):
+    """transpose matrix"""
 
     return [[row[zero] for row in matrix] for zero in range(len(matrix[0]))]
