6f0fc072e07427ae02c7c99f00622eef798a6456
diego0096 <dfqz93@hotmail.com>
2020-07-31 02:23:22 +0000

    indentation

--- math/0x03-probability/normal.py
+++ math/0x03-probability/normal.py
@@ -36,7 +36,7 @@ class Normal:
                 (2 * 3.1415926536 * self.stddev ** 2) ** .5)
 
     def cdf(self, x):
-        """calculate cdf"""
+        """Calculate cdf at a given x value"""
         e = (x - self.mean) / (self.stddev * 2 ** .5)
-        return (1 + (e - e ** 3 / 3 + e ** 5 / 10 - e ** 7
-                        / 42 + e ** 9 / 216) * 2 / 3.1415926536 ** .5) / 2
+        return (1 + (e - e ** 3 / 3 + e ** 5 / 10 - e ** 7 /
+                     42 + e ** 9 / 216) * 2 / 3.1415926536 ** .5) / 2
