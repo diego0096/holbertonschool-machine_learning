da7f7fb6cca4dac07e45c0c8a045bf28006a2df1
diego0096 <dfqz93@hotmail.com>
2020-08-10 03:06:48 +0000

    fixed

--- supervised_learning/0x00-binary_classification/16-deep_neural_network.py
+++ supervised_learning/0x00-binary_classification/16-deep_neural_network.py
@@ -1,36 +1,43 @@
 #!/usr/bin/env python3
-"""Simple deep neural network class"""
+"""Class DeepNeuralNetwork"""
 
 
 import numpy as np
 
 
 class DeepNeuralNetwork:
-    """Simple deep neural network class"""
+    """ Class """
 
     def __init__(self, nx, layers):
-        """
-        nx: number of input features
-        layers: list of number of nodes in each layer
-        """
-        if type(nx) is not int:
-            raise TypeError("nx must be an integer")
+        """Initialize NeuralNetwork"""
+        if not isinstance(nx, int):
+            raise TypeError('nx must be an integer')
+
         if nx < 1:
-            raise ValueError("nx must be a positive integer")
-        if type(layers) is not list or len(layers) < 1:
-            raise TypeError("layers must be a list of positive integers")
-        for layer in layers:
-            if type(layer) is not int or layer < 1:
-                raise TypeError("layers must be a list of positive integers")
+            raise ValueError('nx must be a positive integer')
+
+        if not isinstance(layers, list):
+            raise TypeError('layers must be a list of positive integers')
+
+        if len(layers) == 0:
+            raise TypeError('layers must be a list of positive integers')
 
         self.L = len(layers)
         self.cache = {}
-        self.weights = {"W1": np.random.randn(layers[0], nx) *
-                        np.sqrt(2 / nx),
-                        "b1": np.zeros((layers[0], 1))}
-        for layer, size in enumerate(layers[1:], 2):
-            cur = "W" + str(layer)
-            self.weights[cur] = (np.random.randn(size, layers[layer - 2]) *
-                                 np.sqrt(2 / layers[layer - 2]))
-            cur = "b" + str(layer)
-            self.weights[cur] = np.zeros((layers[layer - 1], 1))
+        self.weights = {}
+
+        for lay in range(len(layers)):
+            if not isinstance(layers[lay], int) or layers[lay] <= 0:
+                raise TypeError('layers must be a list of positive integers')
+
+            self.weights["b" + str(lay + 1)] = np.zeros((layers[lay], 1))
+
+            if lay == 0:
+                sq = np.sqrt(2 / nx)
+                he_et_al = np.random.randn(layers[lay], nx) * sq
+                self.weights["W" + str(lay + 1)] = he_et_al
+
+            else:
+                sq = np.sqrt(2 / layers[lay - 1])
+                he_et_al = np.random.randn(layers[lay], layers[lay - 1]) * sq
+                self.weights["W" + str(lay + 1)] = he_et_al
