d298b5331b8362f2aa8ea43a53213642535960a8
diego0096 <dfqz93@hotmail.com>
2020-08-10 03:55:34 +0000

    eighteen

---
+++ supervised_learning/0x00-binary_classification/18-deep_neural_network.py
@@ -0,0 +1,79 @@
+#!/usr/bin/env python3
+"""Class DeepNeuralNetwork"""
+
+
+import numpy as np
+
+
+class DeepNeuralNetwork:
+    """ Class """
+    def __init__(self, nx, layers):
+        """Initialize NeuralNetwork"""
+
+        if not isinstance(nx, int):
+            raise TypeError('nx must be an integer')
+
+        if nx < 1:
+            raise ValueError('nx must be a positive integer')
+
+        if not isinstance(layers, list):
+            raise TypeError('layers must be a list of positive integers')
+
+        if len(layers) == 0:
+            raise TypeError('layers must be a list of positive integers')
+
+        # Privatizing attributes
+
+        self.__L = len(layers)
+        self.__cache = {}
+        self.__weights = {}
+
+        for lay in range(len(layers)):
+            if not isinstance(layers[lay], int) or layers[lay] <= 0:
+                raise TypeError('layers must be a list of positive integers')
+
+            self.weights["b" + str(lay + 1)] = np.zeros((layers[lay], 1))
+
+            if lay == 0:
+                sq = np.sqrt(2 / nx)
+                he_et_al = np.random.randn(layers[lay], nx) * sq
+                self.weights["W" + str(lay + 1)] = he_et_al
+
+            else:
+                sq = np.sqrt(2 / layers[lay - 1])
+                he_et_al = np.random.randn(layers[lay], layers[lay - 1]) * sq
+                self.weights["W" + str(lay + 1)] = he_et_al
+
+    @property
+    def L(self):
+        """getter method"""
+        return self.__L
+
+    @property
+    def cache(self):
+        """getter method"""
+        return self.__cache
+
+    @property
+    def weights(self):
+        """getter method"""
+        return self.__weights
+
+    def forward_prop(self, X):
+        """Calculates the forward propagation of the neural network"""
+
+        self.__cache["A0"] = X
+
+        for layer in range(self.__L):
+
+            weights = self.__weights["W" + str(layer + 1)]
+            a_ = self.__cache["A" + str(layer)]
+            b = self.__weights["b" + str(layer + 1)]
+
+            z = np.matmul(weights, a_) + b
+
+            forward_prop = 1 / (1 + np.exp(-1 * z))
+
+            self.__cache["A" + str(layer + 1)] = forward_prop
+
+        return self.__cache["A" + str(self.__L)], self.__cache
