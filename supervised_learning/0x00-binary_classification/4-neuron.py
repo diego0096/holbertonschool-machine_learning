54f2eea01d0eb589b99f8c604d49fb356a633bbb
diego0096 <dfqz93@hotmail.com>
2020-08-10 04:28:02 +0000

    four

--- supervised_learning/0x00-binary_classification/4-neuron.py
+++ supervised_learning/0x00-binary_classification/4-neuron.py
@@ -21,25 +21,31 @@ class Neuron:
 
     @property
     def W(self):
+        """Return weights"""
         return self.__W
 
     @property
     def b(self):
+        """Return bias"""
         return self.__b
 
     @property
     def A(self):
+        """Return activation values"""
         return self.__A
 
     def forward_prop(self, X):
+        """Update and return sigmoid activation values"""
         self.__A = 1 / (1 + np.exp(-1 * (np.dot(self.__W, X) + self.__b)))
         return self.__A
 
     def cost(self, Y, A):
+        """Calculate cost using logistic regression"""
         costsum = 0
         return -(Y * np.log(A) + (1 - Y) * np.log(1.0000001 - A)).mean()
 
     def evaluate(self, X, Y):
+        """Evaluate neuron's predictions"""
         A = np.ndarray((1, X.shape[1]))
         A[0] = self.forward_prop(X)
         return np.round(A).astype(int), self.cost(Y, A)
