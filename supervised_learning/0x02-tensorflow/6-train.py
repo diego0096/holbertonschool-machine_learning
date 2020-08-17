06ad10761d70b4aad273e083a9e3f53d687417e8
diego0096 <dfqz93@hotmail.com>
2020-08-17 04:27:06 +0000

    train

--- supervised_learning/0x02-tensorflow/6-train.py
+++ supervised_learning/0x02-tensorflow/6-train.py
@@ -1,7 +1,5 @@
 #!/usr/bin/env python3
-"""
-Train our network
-"""
+"""Train our network"""
 
 
 import tensorflow as tf
@@ -14,9 +12,7 @@ forward_prop = __import__('2-forward_prop').forward_prop
 
 def train(X_train, Y_train, X_valid, Y_valid, layer_sizes,
           activations, alpha, iterations, save_path='/tmp/model.ckpt'):
-    """
-    Train our network
-    """
+    """Train our network"""
     x, y = create_placeholders(X_train.shape[1], Y_train.shape[1])
     y_pred = forward_prop(x, layer_sizes, activations)
     loss = calculate_loss(y, y_pred)
