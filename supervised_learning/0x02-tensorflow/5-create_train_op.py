ce80adc4b61a3a7421374ee54fb6da8ccbc8a685
diego0096 <dfqz93@hotmail.com>
2020-08-17 03:04:53 +0000

    create train op

---
+++ supervised_learning/0x02-tensorflow/5-create_train_op.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""Create training op for our network"""
+
+
+import tensorflow as tf
+
+
+def create_train_op(loss, alpha):
+    """Create training op for our network"""
+    return tf.train.GradientDescentOptimizer(alpha).minimize(loss)
