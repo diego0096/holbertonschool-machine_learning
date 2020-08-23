d6d1c03333a1e301c7cfad9c6949fe0e8a1412c8
diego0096 <dfqz93@hotmail.com>
2020-08-23 21:52:04 +0000

    momentum

--- supervised_learning/0x03-optimization/6-momentum.py
+++ supervised_learning/0x03-optimization/6-momentum.py
@@ -8,8 +8,8 @@ import tensorflow as tf
 def create_momentum_op(loss, alpha, beta1):
     """Creates the training operation for a neural network in tensorflow"""
 
-    α = alpha
-    β1 = beta1
-    train = tf.train.MomentumOptimizer(learning_rate=α, momentum=β1)
+    a = alpha
+    b1 = beta1
+    train = tf.train.MomentumOptimizer(learning_rate=a, momentum=b1)
     op = train.minimize(loss)
     return op
