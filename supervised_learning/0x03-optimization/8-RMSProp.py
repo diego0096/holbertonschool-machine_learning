8391463eac408b24e6a68839306f29c787b8b786
diego0096 <dfqz93@hotmail.com>
2020-08-23 21:13:09 +0000

    RMSProp

---
+++ supervised_learning/0x03-optimization/8-RMSProp.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""Module used to"""
+
+import tensorflow as tf
+
+
+def create_RMSProp_op(loss, alpha, beta2, epsilon):
+    """Updates a variable using the RMSProp optimization algorithm"""
+
+    α = alpha
+    β2 = beta2
+    ε = epsilon
+    train = tf.train.RMSPropOptimizer(learning_rate=α, decay=β2, epsilon=ε)
+    op = train.minimize(loss)
+    return op
