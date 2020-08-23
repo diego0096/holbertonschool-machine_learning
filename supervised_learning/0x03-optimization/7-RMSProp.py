0df2fb6059331e7ecfef7167b2739902ad852bd4
diego0096 <dfqz93@hotmail.com>
2020-08-23 17:36:27 +0000

    RMSProp

---
+++ supervised_learning/0x03-optimization/7-RMSProp.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""Module used to"""
+
+
+def update_variables_RMSProp(alpha, beta2, epsilon, var, grad, s):
+    """Updates a variable using the RMSProp optimization algorithm"""
+    α = alpha
+    β2 = beta2
+    ε = epsilon
+    α = alpha
+    dw = grad
+    w = var
+    s_new = β2 * s + (1 - β2) * (dw * dw)
+    W = w - α * (dw / ((s_new ** 0.5) + ε))
+    return W, s_new
