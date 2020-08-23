4e628549df55ddb2365dedbd8ea519b914bee3a1
diego0096 <dfqz93@hotmail.com>
2020-08-23 16:32:28 +0000

    momentum

---
+++ supervised_learning/0x03-optimization/5-momentum.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""Update a variable with gradient descent"""
+
+
+def update_variables_momentum(alpha, beta1, var, grad, v):
+    """Update a variable with gradient descent"""
+    return None
