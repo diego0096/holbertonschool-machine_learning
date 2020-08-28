71aacecb478bac42576afc1bba454bf8e33defaa
diego0096 <dfqz93@hotmail.com>
2020-08-28 17:08:53 +0000

    gradient descent

--- supervised_learning/0x05-regularization/1-l2_reg_gradient_descent.py
+++ supervised_learning/0x05-regularization/1-l2_reg_gradient_descent.py
@@ -1,26 +1,23 @@
 #!/usr/bin/env python3
-"""Module used to"""
+"""Gradient Descent with L2 Regularization"""
 
 
 import numpy as np
 
 
 def l2_reg_gradient_descent(Y, weights, cache, alpha, lambtha, L):
-    """Function that updates the weights and biases"""
-    weights2 = weights.copy()
-    m = Y.shape[1]
-    for net_ly in reversed(range(L)):
-        n = net_ly + 1
-        if (n == L):
-            dz = cache["A" + str(n)] - Y
-            dw = (np.matmul(cache["A" + str(net_ly)], dz.T) / m).T
+    """function that updates the weights and biases"""
+    weights_copy = weights.copy()
+    for i in range(L, 0, -1):
+        m = Y.shape[1]
+        if i != L:
+            dZi = np.multiply(np.matmul(
+                weights_copy['W' + str(i + 1)].T, dZi
+            ), 1 - cache['A' + str(i)] ** 2)
         else:
-            dz1 = np.matmul(weights2["W" + str(n + 1)].T, current_dz)
-            dz2 = 1 - cache["A" + str(n)]**2
-            dz = dz1 * dz2
-            dw = np.matmul(dz, cache["A" + str(net_ly)].T) / m
-        dw_reg = dw + (lambtha / m) * weights2["W" + str(n)]
-        db = np.sum(dz, axis=1, keepdims=True) / m
-        weights["W" + str(n)] -= (alpha * dw_reg)
-        weights["b" + str(n)] -= (alpha * db)
-        current_dz = dz
+            dZi = cache['A' + str(i)] - Y
+        dWi = np.matmul(dZi, cache['A' + str(i - 1)].T) / m
+        dbi = np.sum(dZi, axis=1, keepdims=True) / m
+        l2 = (1 - alpha * lambtha / m)
+        weights['W' + str(i)] = l2 * weights_copy['W' + str(i)] - alpha * dWi
+        weights['b' + str(i)] = weights_copy['b' + str(i)] - alpha * dbi
