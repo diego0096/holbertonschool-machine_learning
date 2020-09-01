752ddbbb753e044098bc5586d67d761673e9fec1
diego0096 <dfqz93@hotmail.com>
2020-09-01 22:37:33 +0000

    sequential

--- supervised_learning/0x06-keras/0-sequential.py
+++ supervised_learning/0x06-keras/0-sequential.py
@@ -1,25 +1,19 @@
 #!/usr/bin/env python3
-"""Module used to"""
+"""Make a neural network with dropout"""
 
 
 import tensorflow.keras as K
 
 
 def build_model(nx, layers, activations, lambtha, keep_prob):
-    """Builds a NN with the Keras library"""
-    l = lambtha
-
-    a_model = K.Sequential()
-    n_layers = len(layers)
-    regularizer = K.regularizers.l2(l)
-    for i in range(n_layers):
-        a_model.add(K.layers.Dense(
-            units=layers[i],
-            input_dim=nx,
-            kernel_regularizer=regularizer,
-            activation=activations[i],
-        )
-        )
-        if i < n_layers - 1:
-            a_model.add(K.layers.Dropout(1 - keep_prob))
-    return a_model
+    """Make a neural network with dropout"""
+    reg = K.regularizers.l2
+    model = K.Sequential()
+    model.add(K.layers.Dense(layers[0], input_shape=(nx,),
+                             activation=activations[0],
+                             kernel_regularizer=reg(lambtha)))
+    for layer, act in zip(layers[1:], activations[1:]):
+        model.add(K.layers.Dropout(1 - keep_prob))
+        model.add(K.layers.Dense(layer, activation=act,
+                                 kernel_regularizer=reg(lambtha)))
+    return model
