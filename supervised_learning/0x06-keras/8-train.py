7e0e627769e0dd1a329980cc08a3601484364965
diego0096 <dfqz93@hotmail.com>
2020-09-02 02:37:00 +0000

    train

---
+++ supervised_learning/0x06-keras/8-train.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+"""Train a model, with save the best iteration"""
+
+
+import tensorflow.keras as K
+
+
+def train_model(network, data, labels, batch_size, epochs,
+                validation_data=None, early_stopping=False, patience=0,
+                learning_rate_decay=False, alpha=0.1, decay_rate=1,
+                save_best=False, filepath=None,
+                verbose=True, shuffle=False):
+    """function that trains a model using mini-batch gradient descent"""
+    callbacks = []
+    if validation_data and early_stopping:
+        early_stop = K.callbacks.EarlyStopping(monitor='val_loss',
+                                               patience=patience)
+        callbacks.append(early_stop)
+    if validation_data and learning_rate_decay:
+
+        def schedule(epoch):
+            """function that takes an epoch index as input"""
+            return alpha / (1 + decay_rate * epoch)
+        lr_decay = K.callbacks.LearningRateScheduler(
+            schedule=schedule, verbose=1)
+        callbacks.append(lr_decay)
+    if save_best:
+        checkpoint = K.callbacks.ModelCheckpoint(
+            filepath=filepath, monitor='val_loss', verbose=0,
+            save_best_only=True, save_weights_only=False,
+            mode='auto', period=1)
+        callbacks.append(checkpoint)
+    history = network.fit(x=data, y=labels,
+                          batch_size=batch_size,
+                          epochs=epochs,
+                          verbose=verbose,
+                          shuffle=shuffle,
+                          validation_data=validation_data,
+                          callbacks=callbacks)
+    return history
