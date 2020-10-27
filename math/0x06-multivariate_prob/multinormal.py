54ebceba22918eca2b554542b105791481ccc04b
diego0096 <dfqz93@hotmail.com>
2020-10-27 02:04:41 +0000

    multinormal

--- math/0x06-multivariate_prob/multinormal.py
+++ math/0x06-multivariate_prob/multinormal.py
@@ -1,34 +1,49 @@
 #!/usr/bin/env python3
-"""Multivariate normal distribution"""
+"""multinormal.py"""
 
 
 import numpy as np
 
 
 class MultiNormal:
-    """Multivariate normal distribution"""
+    """Multivariate Normal distribution"""
     def __init__(self, data):
-        if type(data) != np.ndarray or len(data.shape) != 2:
-            raise TypeError("data must be a 2D numpy.ndarray")
+        """constructor"""
+        err_1 = "data must be a 2D numpy.ndarray"
+        if not isinstance(data, np.ndarray):
+            raise TypeError(err_1)
+        if data.ndim != 2:
+            raise TypeError(err_1)
+        err_2 = "data must contain multiple data points"
         if data.shape[1] < 2:
-            raise ValueError("data must contain multiple data points")
-        self.mean = np.mean(data, axis=1, keepdims=True)
-        self.cov = np.ndarray((data.shape[0], data.shape[0]))
-        for x in range(data.shape[0]):
-            for y in range(data.shape[0]):
-                self.cov[x][y] = (((data[x] - self.mean[x]) *
-                                   (data[y] - self.mean[y])).sum() /
-                                  (data.shape[1] - 1))
+            raise ValueError(err_2)
+        self.mean, self.cov = self.mean_cov(data)
+
+    @staticmethod
+    def mean_cov(X):
+        """function that calculates the mean and covariance matrix"""
+        d = X.shape[0]
+        n = X.shape[1]
+        mean = np.mean(X, axis=1)
+        mean = mean[..., np.newaxis]
+        X = X - mean
+        cov = np.matmul(X, X.T) / (n - 1)
+        return mean, cov
 
     def pdf(self, x):
-        """Return pdf of the multivariate normal distribution"""
-        if type(x) is not np.ndarray:
-            raise TypeError("x must be a numpy.ndarray")
-        if ((len(x.shape) != 2 or x.shape[1] != 1
-             or x.shape[0] != self.mean.shape[0])):
-            raise ValueError("x must have the shape ({}, 1)"
-                             .format(self.mean.shape[0]))
-        res = np.matmul((x - self.mean).T, np.linalg.inv(self.cov))
-        res = np.exp(np.matmul(res, (x - self.mean)) / -2)
-        res /= np.sqrt(pow(2 * np.pi, x.shape[0]) * np.linalg.det(self.cov))
-        return res[0][0]
+        """function that calculates the PDF at a data point"""
+        err_1 = "x must be a numpy.ndarray"
+        if not isinstance(x, np.ndarray):
+            raise TypeError(err_1)
+        d = self.cov.shape[0]
+        err_2 = "x must have the shape ({}, 1)".format(d)
+        if x.ndim != 2:
+            raise ValueError(err_2)
+        if x.shape[1] != 1 or x.shape[0] != d:
+            raise ValueError(err_2)
+        A = 1.0 / np.sqrt(((2 * np.pi) ** d) * np.linalg.det(self.cov))
+        B = np.exp(-0.5 * np.linalg.multi_dot([(x - self.mean).T,
+                                               np.linalg.inv(self.cov),
+                                               (x - self.mean)]))
+        PDF = A * B
+        return float(PDF)
