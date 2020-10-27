585c23eb76b166c52a81f4b5036fd6bd63f23017
diego0096 <dfqz93@hotmail.com>
2020-10-27 02:00:12 +0000

    multinormal

--- math/0x06-multivariate_prob/multinormal.py
+++ math/0x06-multivariate_prob/multinormal.py
@@ -19,3 +19,16 @@ class MultiNormal:
                 self.cov[x][y] = (((data[x] - self.mean[x]) *
                                    (data[y] - self.mean[y])).sum() /
                                   (data.shape[1] - 1))
+
+    def pdf(self, x):
+        """Return pdf of the multivariate normal distribution"""
+        if type(x) is not np.ndarray:
+            raise TypeError("x must be a numpy.ndarray")
+        if ((len(x.shape) != 2 or x.shape[1] != 1
+             or x.shape[0] != self.mean.shape[0])):
+            raise ValueError("x must have the shape ({}, 1)"
+                             .format(self.mean.shape[0]))
+        res = np.matmul((x - self.mean).T, np.linalg.inv(self.cov))
+        res = np.exp(np.matmul(res, (x - self.mean)) / -2)
+        res /= np.sqrt(pow(2 * np.pi, x.shape[0]) * np.linalg.det(self.cov))
+        return res[0][0]
