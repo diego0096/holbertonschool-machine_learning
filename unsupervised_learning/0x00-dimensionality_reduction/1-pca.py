0564adcb815cc8867b0c173e4eacf237b4eee897
diego0096 <dfqz93@hotmail.com>
2020-10-29 00:15:40 +0000

    pca

--- unsupervised_learning/0x00-dimensionality_reduction/1-pca.py
+++ unsupervised_learning/0x00-dimensionality_reduction/1-pca.py
@@ -1,13 +1,16 @@
 #!/usr/bin/env python3
-"""1-pca.py"""
+"""PCA: principal components analysis"""
 
 
 import numpy as np
 
 
 def pca(X, ndim):
-    """function that performs principal components"""
-    X = X - np.mean(X, axis=0)
-    U, S, Vt = np.linalg.svd(X)
-    Tr = np.matmul(U[..., :ndim], np.diag(S[..., :ndim]))
-    return Tr
+    """Function that performs PCA on a dataset"""
+    normal = np.mean(X, axis=0)
+    X_normal = X - normal
+    vh = np.linalg.svd(X_normal)[2]
+    Weights_r = vh[: ndim].T
+    T = np.matmul(X_normal, Weights_r)
+
+    return T
