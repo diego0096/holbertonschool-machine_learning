85fc65814433135541a417820bac8db90b816a55
diego0096 <dfqz93@hotmail.com>
2020-11-07 02:28:02 +0000

    kmeans

---
+++ unsupervised_learning/0x01-clustering/10-kmeans.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""10-kmeans.py"""
+
+
+import sklearn.cluster
+
+
+def kmeans(X, k):
+    """function that performs K-means on a dataset"""
+    kmeans = sklearn.cluster.KMeans(n_clusters=k).fit(X)
+    C = kmeans.cluster_centers_
+    clss = kmeans.labels_
+    return C, clss
