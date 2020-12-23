b2805734e206b97c313f0046a7517eafc7d9519a
diego0096 <dfqz93@hotmail.com>
2020-12-23 14:09:06 +0000

    uni_bleu

--- supervised_learning/0x10-nlp_metrics/0-uni_bleu.py
+++ supervised_learning/0x10-nlp_metrics/0-uni_bleu.py
@@ -1,53 +1,37 @@
 #!/usr/bin/env python3
-"""2-cumulative_bleu.py"""
+"""This script has the method uni_bleu"""
 
 
 import numpy as np
 
 
 def uni_bleu(references, sentence):
-    """function that calculates the unigram BLEU score"""
-    if not isinstance(sentence, list):
-        sentence = sentence.lower()
-        sentence = np.array(sentence.split())
-    else:
-        sentence = np.array([word.lower() for word in sentence])
-    sentence_len = sentence.shape[0]
-    if not np.all([isinstance(reference, list) for reference in references]):
-        references = [reference.split() for reference in references]
-        references = [np.array(reference.split()) for reference in references]
-    else:
-        references = [np.array([word.lower() for word in reference])
-                      for reference in references]
-    references_len = [reference.shape[0] for reference in references]
-    clipped_precision_score = 0
-    sentence_n_grams = sentence
-    unique, counts = np.unique(sentence_n_grams, return_counts=True)
-    sentence_n_grams_dict = dict(zip(unique, counts))
-    references_n_grams_dicts = []
+    """This method has the calculates the unigram BLEU score"""
+    uniques = list(set(sentence))
+    dict_words = {}
     for reference in references:
-        reference_n_grams = reference
-        unique, counts = np.unique(reference_n_grams, return_counts=True)
-        reference_n_grams_dict = dict(zip(unique, counts))
-        references_n_grams_dicts.append(reference_n_grams_dict)
-    count = sum(sentence_n_grams_dict.values())
-    for n_gram in sentence_n_grams_dict.keys():
-        references_n_gram_count = [reference_n_grams_dict[n_gram]
-                                   for reference_n_grams_dict
-                                   in references_n_grams_dicts
-                                   if n_gram in reference_n_grams_dict]
-        if not len(references_n_gram_count):
-            keep_max = 0
-        else:
-            keep_max = max(references_n_gram_count)
-        if (sentence_n_grams_dict[n_gram] > keep_max):
-            sentence_n_grams_dict[n_gram] = keep_max
-    clipped_count = sum(sentence_n_grams_dict.values())
-    clipped_precision_score += (clipped_count / count)
-    if sentence_len > np.min(references_len):
-        BP = 1
+        for word in reference:
+            if word in uniques:
+                if word not in dict_words.keys():
+                    dict_words[word] = reference.count(word)
+                else:
+                    actual = reference.count(word)
+                    prev = dict_words[word]
+                    dict_words[word] = max(actual, prev)
+    len_cand = len(sentence)
+    prob = sum(dict_words.values()) / len_cand
+    best_match_tuples = []
+    for reference in references:
+        ref_len = len(reference)
+        diff = abs(ref_len - len_cand)
+        best_match_tuples.append((diff, ref_len))
+    sort_tuples = sorted(best_match_tuples, key=lambda x: x[0])
+    best_match = sort_tuples[0][1]
+    if len_cand > best_match:
+        bp = 1
     else:
-        bp = 1 - (np.min(references_len) / sentence_len)
-        BP = np.exp(bp)
-    BLEU = BP * clipped_precision_score
-    return BLEU
+        bp = np.exp(1 - (best_match / len_cand))
+    Bleu_score = bp * np.exp(np.log(prob))
+    if Bleu_score > 0.4:
+        return round(Bleu_score, 7)
+    return Bleu_score
