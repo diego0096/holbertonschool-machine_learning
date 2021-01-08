ceb3a6c02d1029c2b49c09fe6ea5fd91dec8e5b5
diego0096 <dfqz93@hotmail.com>
2021-01-08 20:16:17 +0000

    tranformer_encoder

---
+++ supervised_learning/0x11-attention/9-transformer_encoder.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+"""9-transformer_encoder.py"""
+
+
+import tensorflow as tf
+positional_encoding = __import__('4-positional_encoding').positional_encoding
+EncoderBlock = __import__('7-transformer_encoder_block').EncoderBlock
+
+
+class Encoder(tf.keras.layers.Layer):
+    """class that instantiates an Encoder"""
+
+    def __init__(self, N, dm, h, hidden, input_vocab, max_seq_len,
+                 drop_rate=0.1):
+        """constructor"""
+        super(Encoder, self).__init__()
+        self.N = N
+        self.dm = dm
+        self.embedding = tf.keras.layers.Embedding(input_vocab, dm)
+        self.positional_encoding = positional_encoding(max_seq_len, dm)
+        self.blocks = [EncoderBlock(dm, h, hidden, drop_rate)
+                       for _ in range(N)]
+        self.dropout = tf.keras.layers.Dropout(drop_rate)
+
+    def call(self, x, training, mask):
+        """function that builds an Encoder"""
+        seq_len = x.shape[1]
+        embeddings = self.embedding(x)
+        embeddings *= tf.math.sqrt(tf.cast(self.dm, tf.float32))
+        embeddings += self.positional_encoding[:seq_len, :]
+        output = self.dropout(embeddings, training=training)
+        for i in range(self.N):
+            output = self.blocks[i](output, training, mask)
+        return output
