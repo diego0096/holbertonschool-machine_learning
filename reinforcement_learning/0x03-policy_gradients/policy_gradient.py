98db722fdec9906311d0075c46a0c65b8192f88f
diego0096 <dfqz93@hotmail.com>
2021-03-05 23:57:31 +0000

    policy_gradient

--- reinforcement_learning/0x03-policy_gradients/policy_gradient.py
+++ reinforcement_learning/0x03-policy_gradients/policy_gradient.py
@@ -10,3 +10,19 @@ def policy(state, weight):
     res = state @ weight
     res = np.exp(res)
     return res / res.sum()
+
+
+def policy_gradient(state, weight):
+    """Compute policy gradient"""
+    state = state.reshape(1, -1)
+    policy_value = policy(state, weight)
+    random = np.random.random()
+    total = 0
+    for i, chance in enumerate(policy_value[0]):
+        total += chance
+        action = i
+        if random < total:
+            break
+    grad = state.T - (policy_value * state.T)
+
+    return action, grad
