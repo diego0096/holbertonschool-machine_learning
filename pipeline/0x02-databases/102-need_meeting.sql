19e9d49391e2cdb3f26be89db5a15ddc5f238d42
diego0096 <dfqz93@hotmail.com>
2021-03-18 00:37:38 +0000

    need_metting

---
+++ pipeline/0x02-databases/102-need_meeting.sql
@@ -0,0 +1,6 @@
+-- Creates a view need_meeting that lists all students
+CREATE VIEW need_meeting
+AS SELECT name
+FROM students
+WHERE score < 80
+  AND (last_meeting IS NULL OR DATEDIFF(CURDATE(), last_meeting) > 30);
